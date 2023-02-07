72a03d20b7b723790087c2af1d5fb1bbfd8e56bd
VasoRuim011 <rafaelsouza478@gmail.com>
2023-02-07 10:07:36 -0300

    ok

--- src/main/java/org/generation/blogpessoal/model/Postagem.java
+++ src/main/java/org/generation/blogpessoal/model/Postagem.java
@@ -37,6 +37,11 @@ public class Postagem {
 	@ManyToOne
 	@JsonIgnoreProperties("postagem")
 	private Tema tema;
+	
+	@ManyToOne
+    @JsonIgnoreProperties("postagem")
+    private Usuario usuario;
+
 
 	public Tema getTema() {
 		return tema;
